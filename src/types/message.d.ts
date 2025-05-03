335a4af9b8a3170c0cb434135bc4861791aa6c2a
Franco Carballar <francocarballar@gmail.com>
2025-05-03 16:25:47 -0300

    feat: :label: añade tipo LanguageCode para la gestión de códigos de idioma

--- src/types/message.d.ts
+++ src/types/message.d.ts
@@ -7,3 +7,39 @@ export interface Messages {
   regionNotFound: string
   internalServerError: string
 }
+
+/**
+ * @type {LanguageCode}
+ * @description Type for the language codes.
+ */
+export type LanguageCode =
+  | 'en'
+  | 'es'
+  | 'zh'
+  | 'hi'
+  | 'pt'
+  | 'bn'
+  | 'ru'
+  | 'ja'
+  | 'pa'
+  | 'vi'
+  | 'tr'
+  | 'ar'
+  | 'mr'
+  | 'te'
+  | 'ko'
+  | 'ta'
+  | 'ur'
+  | 'de'
+  | 'id'
+  | 'fr'
+  | 'jv'
+  | 'fa'
+  | 'it'
+  | 'ha'
+  | 'gu'
+  | 'bho'
+  | 'th'
+  | 'nl'
+  | 'yo'
+  | 'uk'
