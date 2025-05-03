e0244032ad4a84f48c8773c165318f54354f7150
Franco Carballar <francocarballar@gmail.com>
2025-05-03 16:24:33 -0300

    feat: :label: añade interfaz Messages para la gestión de mensajes de error

---
+++ src/types/message.d.ts
@@ -0,0 +1,9 @@
+/**
+ * @interface Messages
+ * @description Interface for the messages.
+ */
+export interface Messages {
+  countryNotFound: string
+  regionNotFound: string
+  internalServerError: string
+}
