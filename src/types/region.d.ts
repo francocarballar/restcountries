b6c8c47647a5dda4a0503900d1c5671708608a5a
Franco Carballar <francocarballar@gmail.com>
2025-05-03 16:34:33 -0300

    feat: :label: añade interfaz RegionSummary para la gestión de resúmenes de regiones

---
+++ src/types/region.d.ts
@@ -0,0 +1,9 @@
+/**
+ * @interface RegionSummary
+ * @description Interface for the region summary.
+ */
+export interface RegionSummary {
+  name: string
+  countryCount: number
+  subregions: string[]
+}
